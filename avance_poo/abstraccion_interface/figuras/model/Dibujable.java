e95ceef17f5c6ae4f9d58293384d65df12870dfb
morlig <vmorlig@gmail.com>
2024-11-28 00:27:14 +0100

    delete abstraccion interface

--- avance_poo/abstraccion_interface/figuras/model/Dibujable.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface Dibujable {
-    public void dibujar();
-}
--- avance_poo/abstraccion_interface/figuras/model/Figura.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public abstract class Figura {
-
-    public abstract float calcularArea();
-
-    public abstract float calcularPerimetro();
-
-}
